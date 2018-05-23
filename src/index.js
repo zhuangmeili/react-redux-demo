f979c276a6c87547fa9e1c792b7120aa523847e9
zhuangmeili <zhuangmeili@auohome.com.cn>
2018-05-23 18:52:30 +0800

    redux-可视化工具

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from './reducers';
-const store=createStore(rootReducer);
+// 安装redux-devtools-extension的可视化工具。
+import { composeWithDevTools } from 'redux-devtools-extension'
+const store=createStore(rootReducer,composeWithDevTools());
 //redux :end
 
 ReactDOM.render(
