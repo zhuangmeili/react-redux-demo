369608fdf83c4ca5ee4efbc2bbfd64020016cf7c
zhuangmeili <zhuangmeili@auohome.com.cn>
2018-06-15 18:43:41 +0800

    [sass]

--- src/pages/Buy/Buy.js
+++ src/pages/Buy/Buy.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-
+import './Buy.scss';
 class Buy extends Component {
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
+      <div className="pages_Buy">
+        <header className="header">
           buy pages
-        </p>
+        </header>
+
+        <ul>
+          <li className="listItem Flex">
+            <img className="left" src="http://pic2.ooopic.com/12/42/25/02bOOOPIC95_1024.jpg" alt=""/>
+            <div className="right Flex1 HackWidth">
+              <h2>这是header</h2>
+              <p></p>
+            </div>
+          </li>
+        </ul>
       </div>
     );
   }
